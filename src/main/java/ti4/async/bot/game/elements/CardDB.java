6cda9bc25ddbbb4f81a9d5ad40d1eb98517e9b2e
shavnir <shavnir@gmail.com>
2022-02-27 11:38:52 -0600

    Update CardDB to be a singleton.  Still doesn't do anything though

--- src/main/java/ti4/async/bot/game/elements/CardDB.java
+++ src/main/java/ti4/async/bot/game/elements/CardDB.java
@@ -2,23 +2,36 @@ package ti4.async.bot.game.elements;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 public class CardDB {
-	
-	private File dbFile;
-	
+
+	private static CardDB instance;
+
+	private EnumMap<CardType, List<Card>> cardDatabase;
+
 	public CardDB(File f) {
-		dbFile = f;
-	}
-	
-	public List<Card> getActionCardList(){
-		// for now dummy data
-		List<Card> rtnVal = new ArrayList<Card>();
-		for(int x=0;x<10;x++) {
-			Card c = new Card(x,"Card " + x,"Generic Card Text", CardType.ACTION_CARD);
-			rtnVal.add(c);
+		// for now dummy data to have something in there!
+		List<Card> actionCardList = new ArrayList<Card>();
+		for (int x = 0; x < 10; x++) {
+			Card c = new Card(x, "Card " + x, "Generic Card Text", CardType.ACTION_CARD);
+			actionCardList.add(c);
 		}
-		return rtnVal;
+		cardDatabase = new EnumMap<>(CardType.class);
+		cardDatabase.put(CardType.ACTION_CARD, actionCardList);
+	}
+
+	public static void initializeSingleton(File f) {
+		// load from file here
+		instance = new CardDB(f);
+	}
+
+	public static CardDB getSingleton() {
+		return instance;
+	}
+
+	public List<Card> getActionCardList() {
+		return cardDatabase.get(CardType.ACTION_CARD);
 	}
 }
